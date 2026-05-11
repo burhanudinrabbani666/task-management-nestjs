8abed784fef9fa4218b6abdc702c27f25f7f7b10
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-05-11 16:16:13 +0700

    Sign in route

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -8,5 +9,16 @@ export class AuthService {
 
   public signUp(authCreadential: AuthCredentialDto): Promise<void> {
     return this.usersRepository.createNewUser(authCreadential);
+  }
+
+  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+    const { username, password } = authCredentialDto;
+    const user = await this.usersRepository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return 'Success';
+    }
+
+    throw new UnauthorizedException('Please Check You login Credentials');
   }
 }
